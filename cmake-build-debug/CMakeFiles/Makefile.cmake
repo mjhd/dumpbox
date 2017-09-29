c976bc091ec7d4cc89c05a7cd43b28648c38ef5d
Matt Houser <mhouser@e1z2r6p10.42.us.org>
2017-09-28 23:36:21 -0700

    CLion shite sorted oot

--- cmake-build-debug/CMakeFiles/Makefile.cmake
+++ cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -48,5 +48,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/codechef.dir/DependInfo.cmake"
+  "CMakeFiles/shitbox.dir/DependInfo.cmake"
   )
---
+++ cmake-build-debug/CMakeFiles/shitbox.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shitbox.dir/aa.c.o"
+  "CMakeFiles/shitbox.dir/bb.c.o"
+  "CMakeFiles/shitbox.dir/dd.c.o"
+  "CMakeFiles/shitbox.dir/ee.c.o"
+  "CMakeFiles/shitbox.dir/ff.c.o"
+  "CMakeFiles/shitbox.dir/gg.c.o"
+  "CMakeFiles/shitbox.dir/read_args_short.c.o"
+  "shitbox.pdb"
+  "shitbox"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/shitbox.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
